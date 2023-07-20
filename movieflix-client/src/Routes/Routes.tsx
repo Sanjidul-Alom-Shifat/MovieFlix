f3f7661381c8e61e74cbf93d3483bae89bb5888a
Sanjidul-Alom-Shifat <sanjidul.alom.shifat.official@gmail.com>
2023-07-20 19:51:01 +0600

    adding navigation bar

--- movieflix-client/src/Routes/Routes.tsx
+++ movieflix-client/src/Routes/Routes.tsx
@@ -9,7 +9,8 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element:<HomePage></HomePage>
+                element: <HomePage></HomePage>
+                
             }
         ]
     }
